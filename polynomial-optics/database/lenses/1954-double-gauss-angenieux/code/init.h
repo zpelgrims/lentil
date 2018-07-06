558f1591352cc27fcfc5cc6fe4b8db27bf32ddf8
Zeno Pelgrims <zenopelgrims@gmail.com>
2018-07-06 07:16:39 -0700

    adding generated headers,c++  test completed

---
+++ polynomial-optics/database/lenses/1954-double-gauss-angenieux/code/init.h
@@ -0,0 +1,9 @@
+static const char *lens_name = "lenses"; // descriptive name of the lens
+static const float lens_outer_pupil_radius = 54.000000; // scene facing radius in mm
+static const float lens_inner_pupil_radius = 35.000000; // sensor facing radius in mm
+static const float lens_length = 182.412003; // overall lens length in mm
+static const float lens_focal_length = 55.742001; // approximate lens focal length in mm (BFL)
+static const float lens_aperture_pos = 64.089996; // distance aperture -> outer pupil in mm
+static const float lens_aperture_housing_radius = 25.500000; // lens housing radius at the aperture
+static const float lens_outer_pupil_curvature_radius = 164.130005; // radius of curvature of the outer pupil
+static const float lens_field_of_view = 0.976445; // cosine of the approximate field of view assuming a 35mm image
