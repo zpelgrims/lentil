6f673f6913d8c236ca166ce30efb99334b4184ad
Zeno Pelgrims <zenopelgrims@gmail.com>
2018-07-12 16:18:34 -0700

    adding panavision rear anamorphic lens, makefile linux

--- polynomial-optics/src/raytrace_draw.h
+++ polynomial-optics/src/raytrace_draw.h
@@ -183,7 +183,7 @@ static inline int evaluate_reverse_draw(const lens_element_t *lenses, const int 
   cairo_line_to(cr, 0.0f, pos[dim_up] - pos[2]*dir[dim_up]/dir[2]);
   
   // print y=0 intersection
-  printf("pos[dim_up] - pos[2]*dir[dim_up]/dir[2]: %f\n", pos[dim_up] - pos[2]*dir[dim_up]/dir[2]);
+  // printf("pos[dim_up] - pos[2]*dir[dim_up]/dir[2]: %f\n", pos[dim_up] - pos[2]*dir[dim_up]/dir[2]);
   
   cairo_save(cr);
   cairo_scale(cr, 1/scale, 1/scale);
