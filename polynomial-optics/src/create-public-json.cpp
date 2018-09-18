48417a3a00365b3ad5bb4ae236d43819a642f042
Zeno Pelgrims <zenopelgrims@gmail.com>
2018-09-17 21:45:40 -0700

    removing comment

--- polynomial-optics/src/create-public-json.cpp
+++ polynomial-optics/src/create-public-json.cpp
@@ -38,29 +38,6 @@ void create_public_json(json lens_database, std::string lens_public_database_pat
   printf("Written public lens database to location: %s\n", lens_public_database_path.c_str());
 }
 
-/*
-    
-    for (auto &element : lens_database){
-      if (element["production-ready"].get<bool>() == false) {
-        to_erase.push_back();
-        continue;
-      }
-
-      if (element.find("optical-elements-adjusted") != element.end()) {
-        element.erase("optical-elements-adjusted");
-      }
-      element.erase("optical-elements-patent");
-      element.erase("patent-location");
-      element.erase("focal-length-mm-raytraced");
-      element.erase("focal-length-mm-patent");
-    }
-    
-    std::ofstream out_json(lens_public_database_path);
-    out_json << std::setw(2) << lens_database << std::endl;
-
-    printf("Written public lens database to location: %s\n", lens_public_database_path.c_str());
-}
-*/
 
 int main(int argc, char *argv[])
 {
