fd66535aebf37b4b7b9ce0908bbdad49337d201c
Zeno Pelgrims <zenopelgrims@gmail.com>
2018-09-17 21:07:31 -0700

    adding copy_to_www.py

---
+++ polynomial-optics/src/copy-images-to-www.py
@@ -0,0 +1,55 @@
+import json
+import os
+import shutil
+import os, errno
+
+class copy_to_www_folder():
+
+    def __init__(self):
+        self.lentil_path = os.environ['LENTIL_PATH']
+        if "polynomial-optics" not in self.lentil_path:
+            raise Exception("$LENTIL_PATH has been set incorrectly")
+
+    def _construct_database_lens_folder(self, lens):
+        return "{}-{}-{}".format(lens["year"], lens["company"], lens["product-name"])
+
+    def _construct_imgs_www_folder(self):
+        return "{}/../www/imgs/lenses/".format(self.lentil_path)
+
+    def remove_image_folder(self):
+        try:
+            print("Removing image folder: {}".format(self._construct_imgs_www_folder()))
+            shutil.rmtree(self._construct_imgs_www_folder())
+        except OSError as e:
+            print("Error: {}".format(e))
+
+    def _mkdir(self, dir):
+        try:
+            os.makedirs(dir)
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
+
+    def execute(self):
+        with open(self.lentil_path + '/database/lenses.json') as file:
+            lens_database = json.load(file)
+
+        for lens in lens_database:
+            if not lens_database[lens]["production-ready"]:
+                continue
+
+            lens_folder = self._construct_database_lens_folder(lens_database[lens])
+            lens_database_image_path = "{0}/database/lenses/{1}/{1}.svg".format(self.lentil_path, lens_folder)
+            lens_www_image_path = "{}/{}/{}.svg".format(self._construct_imgs_www_folder(), lens, lens_folder)
+            
+            self._mkdir("{}/{}".format(self._construct_imgs_www_folder(), lens))
+
+            try:
+                shutil.copyfile(lens_database_image_path, lens_www_image_path)
+            except IOError as e:
+                print("No image to copy for lens: {}".format(lens))
+                continue
+
+to_www = copy_to_www_folder()
+to_www.remove_image_folder()
+to_www.execute()
--- polynomial-optics/src/create-public-json.cpp
+++ polynomial-optics/src/create-public-json.cpp
@@ -7,27 +7,33 @@
 using json = nlohmann::json;
 
 
+void create_public_json(json lens_database, std::string lens_public_database_path) {
+  for (auto &element : lens_database)
+    {
+      if (element.find("optical-elements-adjusted") != element.end()) {
+        element.erase("optical-elements-adjusted");
+      }
+      element.erase("optical-elements-patent");
+      element.erase("patent-location");
+      element.erase("focal-length-mm-raytraced");
+      element.erase("focal-length-mm-patent");
+    }
+    
+    std::ofstream out_json(lens_public_database_path);
+    out_json << std::setw(2) << lens_database << std::endl;
+
+    printf("Written public lens database to location: %s\n", lens_public_database_path.c_str());
+}
+
+
 int main(int argc, char *argv[])
 {
-  const char *id = argv[1];
-
+  
   std::string lentil_path = std::getenv("LENTIL_PATH");
   std::string lens_public_database_path = lentil_path + "/../www/json/lenses_public.json";
   std::string lens_database_path = lentil_path + "/database/lenses.json";
   std::ifstream in_json(lens_database_path);
   json lens_database = json::parse(in_json);
 
-  for(auto& element : lens_database)
-  {
-    if (element.find("optical-elements-adjusted") != element.end()) {
-      element.erase("optical-elements-adjusted");
-    }
-    element.erase("optical-elements-patent");
-    element.erase("patent-location");
-  }
-  
-  std::ofstream out_json(lens_public_database_path);
-  out_json << std::setw(2) << lens_database << std::endl;
-
-  printf("Written public lens database to location: %s\n", lens_public_database_path.c_str());
+  create_public_json(lens_database, lens_public_database_path);
 }
