d77f1849a25b3997d19d16ddd088afa84378af21
Zeno Pelgrims <zenopelgrims@gmail.com>
2018-10-06 18:03:43 -0700

    adding zip and timezone

--- scripts/server-setup-ubuntu.sh
+++ scripts/server-setup-ubuntu.sh
@@ -1,10 +1,12 @@
+#!/bin/bash
 # server-setup-ubuntu.sh
 
 # set timezone, should be equal between all build servers regardless of location
+sudo timedatectl set-timezone UTC
 
 # create home folder
 mkdir ~/lentil-build/
-export LENTIL_BUILD_HOME=~/lentil-build/
+export LENTIL_BUILD_HOME=~/lentil-build
 mkdir ~/lentil-build/builds/
 cd ~/lentil-build/
 
@@ -17,6 +19,7 @@ wget -O ~/lentil-build/arnold.zip https://www.dropbox.com/sh/rx57g4e3reamk3i/AAD
 
 # unzip arnold sdk's
 sudo apt install unzip
+sudo apt install zip
 unzip ~/lentil-build/arnold.zip -d ~/lentil-build/arnold
 rm -f ~/lentil-build/arnold.zip
 export LENTIL_ARNOLD_SDKS=~/lentil-build/arnold
