fad071c29d98856aac6618c4a0c52b94655b8b22
root <zenopelgrims@gmail.com>
2018-11-04 15:35:17 +0000

    adding fitted schneider anamorphic (#2004)

---
+++ polynomial-optics/database/lenses/2001-Schneider-anamorphic/31/code/lens_constants.h
@@ -0,0 +1,18 @@
+case Schneider_anamorphic_2001_31mm:
+{
+camera->lens_name = "Schneider-anamorphic-2001-31mm"; // descriptive name of the lens
+camera->lens_outer_pupil_radius = 30.607374; // scene facing radius in mm
+camera->lens_inner_pupil_radius = 14.283442; // sensor facing radius in mm
+camera->lens_inner_pupil_curvature_radius = -54.277077; // radius of curvature of the inner pupil
+camera->lens_outer_pupil_curvature_radius = -103.350899; // radius of curvature of the outer pupil
+camera->lens_length = 185.155121; // overall lens length in mm
+camera->lens_back_focal_length = 44.259186; // approximate lens back focal length in mm
+camera->lens_effective_focal_length = 31.000000; // effective focal length in mm
+camera->lens_aperture_pos = 125.745293; // distance aperture -> outer pupil in mm
+camera->lens_aperture_housing_radius = 10.712581; // lens housing radius at the aperture
+camera->lens_outer_pupil_geometry = "cyl-y"; // geometry of outer pupil
+camera->lens_inner_pupil_geometry = "spherical"; // geometry of inner pupil
+camera->lens_fstop = 1.954038; // effective_focal_length/(2*aperture_housing_radius)
+camera->lens_aperture_radius_at_fstop = 9.802764; // aperture radius at smallest fstop
+camera->lens_field_of_view = 0.421175; // cosine of the approximate field of view assuming a 35mm image
+} break;
