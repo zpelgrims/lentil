4d6addce556462078846478b3cd6c1d660f997cb
Zeno Pelgrims <zenopelgrims@gmail.com>
2018-12-03 09:52:59 -0800

    adding comment where at bug location

--- polynomial-optics/src/gencode.c
+++ polynomial-optics/src/gencode.c
@@ -122,6 +122,8 @@ int main(int argc, char **argv)
   fprintf(f, "camera->lens_aperture_housing_radius = %f; // lens housing radius at the aperture\n", aperture_housing_radius);
   fprintf(f, "camera->lens_outer_pupil_geometry = \"%s\"; // geometry of outer pupil\n", lenses[0].geometry.c_str());
   fprintf(f, "camera->lens_inner_pupil_geometry = \"%s\"; // geometry of inner pupil\n", lenses[lenses_cnt-1].geometry.c_str());
+
+  // these seem wrong, should be relative to the focal length, not a constant for all focal lengths
   fprintf(f, "camera->lens_fstop = %f; // effective_focal_length/(2*aperture_housing_radius)\n", lens_database[id]["fstop"].get<float>());
   fprintf(f, "camera->lens_aperture_radius_at_fstop = %f; // aperture radius at smallest fstop\n", lens_database[id]["max-fstop-aperture-radius"].get<float>());
 
