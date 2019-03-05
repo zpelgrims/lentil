a902ef38ddd7e5cf3f8b8e913fffcb8da524e951
Zeno Pelgrims <zenopelgrims@gmail.com>
2019-03-04 19:46:35 -0800

    update

--- polynomial-optics/build.sh
+++ polynomial-optics/build.sh
@@ -4,10 +4,6 @@
 
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
-# todo:
-# check if dependencies are installed
-# if not, install all dependencies
-
 echo ""
 echo "Setting environment variables: "
     # green text color
@@ -16,8 +12,8 @@ echo "Setting environment variables: "
     echo -e "\t LENTIL_PATH: " $LENTIL_PATH
 
     # add dependency packages environment variables
-    export PKG_CONFIG_PATH=/usr/lib64/pkgconfig/:$PKG_CONFIG_PATH
-    #export PKG_CONFIG_PATH=/opt/X11/lib/pkgconfig/:$PKG_CONFIG_PATH
+    #export PKG_CONFIG_PATH=/usr/lib64/pkgconfig/:$PKG_CONFIG_PATH
+    export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig/:$PKG_CONFIG_PATH
     echo -e "\t PKG_CONFIG_PATH: " $PKG_CONFIG_PATH
 echo ""
 
