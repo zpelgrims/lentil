0a7124b29ab6a27c45346ee3620055be9713f8f1
Zeno Pelgrims <zenopelgrims@gmail.com>
2019-05-11 14:40:15 -0700

    don't create unnecessary download user folder, check if zip exists

--- www/sh/build_on_build_server.sh
+++ www/sh/build_on_build_server.sh
@@ -4,9 +4,12 @@ USER=testemail@gmail.com &&
 USER_BUILD_FOLDER=$DATE-$USER &&
 DOWNLOAD_DIR=/root/test_upload_folder &&
 
-mkdir -p $DOWNLOAD_DIR/$USER_BUILD_FOLDER &&
+mkdir -p $DOWNLOAD_DIR &&
 
 ssh -t $BUILD_SERVER "cd lentil-build/lentil/pota && bash ./build_server.sh .1001 $USER $USER_BUILD_FOLDER $DOWNLOAD_DIR" &&
 
 # pass the .zip back to the user
-url = $DOWNLOAD_DIR/$USER_BUILD_FOLDER.zip
+url=$DOWNLOAD_DIR/$USER_BUILD_FOLDER.zip
+if [ ! -f $url ]; then
+    echo "The .zip was not created!"
+fi
