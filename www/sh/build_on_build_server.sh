9761ffd40ece2d0c00ebdeb84c75369373b482ed
Zeno Pelgrims <zenopelgrims@gmail.com>
2019-05-11 15:31:37 -0700

    makefile location update

--- www/sh/build_on_build_server.sh
+++ www/sh/build_on_build_server.sh
@@ -6,7 +6,7 @@ DOWNLOAD_DIR=/root/test_upload_folder &&
 
 mkdir -p $DOWNLOAD_DIR &&
 
-ssh -t $BUILD_SERVER "cd lentil-build/lentil/pota && bash ./build_server.sh .1001 $USER $USER_BUILD_FOLDER $DOWNLOAD_DIR" &&
+ssh -t $BUILD_SERVER "cd lentil-build/lentil/pota/build/server && bash ./build_server.sh .1001 $USER $USER_BUILD_FOLDER $DOWNLOAD_DIR" &&
 
 # pass the .zip back to the user
 url=$DOWNLOAD_DIR/$USER_BUILD_FOLDER.zip
