900f1f84222885cd091605eb89faec1f97a51096
Zeno Pelgrims <zenopelgrims@gmail.com>
2019-09-04 19:36:58 -0700

    adding 2 lenses, remove on-demand compile

---
+++ polynomial-optics/database/lenses/1978-minolta-fisheye/16/code/lens_constants.h
@@ -0,0 +1,18 @@
+case minolta__fisheye__1978__16mm:
+{
+camera->lens_name = "minolta-fisheye-1978-16mm"; // descriptive name of the lens
+camera->lens_outer_pupil_radius = 24.104868; // scene facing radius in mm
+camera->lens_inner_pupil_radius = 9.974428; // sensor facing radius in mm
+camera->lens_inner_pupil_curvature_radius = -24.184663; // radius of curvature of the inner pupil
+camera->lens_outer_pupil_curvature_radius = 78.187877; // radius of curvature of the outer pupil
+camera->lens_length = 84.508338; // overall lens length in mm
+camera->lens_back_focal_length = 38.734027; // approximate lens back focal length in mm
+camera->lens_effective_focal_length = 16.000000; // effective focal length in mm
+camera->lens_aperture_pos = 32.192466; // distance aperture -> outer pupil in mm
+camera->lens_aperture_housing_radius = 7.480821; // lens housing radius at the aperture
+camera->lens_outer_pupil_geometry = "spherical"; // geometry of outer pupil
+camera->lens_inner_pupil_geometry = "spherical"; // geometry of inner pupil
+camera->lens_fstop = 2.663973; // effective_focal_length/(2*aperture_housing_radius)
+camera->lens_aperture_radius_at_fstop = 7.160060; // aperture radius at smallest fstop
+camera->lens_field_of_view = 0.099429; // cosine of the approximate field of view assuming a 35mm image
+} break;
