10557fcbb3cef29bf90294b026452084837123a6
Zeno Pelgrims <zenopelgrims@gmail.com>
2019-09-07 10:40:37 -0700

    adding 0004 28mm

---
+++ polynomial-optics/src/unit_render_arnold.py
@@ -0,0 +1,19 @@
+from arnold import *
+import os
+import sys
+
+# read all production-ready lenses & focal lengths
+# construct correct name in shader
+# load "standard" ass
+# look for camera node
+# change lens parameter on that node
+# render
+# output to correct directory with correct name
+
+# take inspiration from: https://docs.arnoldrenderer.com/display/ARP/Updating+and+re-rendering+with+python
+
+def rerender_all_unit_render_lenses():
+    pass
+
+def unit_render_lens():
+    pass
