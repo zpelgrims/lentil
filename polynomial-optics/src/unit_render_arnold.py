ca7b28c68bf784d8d895eb860de5986b3b0e2879
Zeno Pelgrims <zenopelgrims@gmail.com>
2019-09-21 09:28:24 -0700

    loops correctly over lenses now

--- polynomial-optics/src/unit_render_arnold.py
+++ polynomial-optics/src/unit_render_arnold.py
@@ -1,9 +1,12 @@
 #TODO: need to build lens before lens render test obviously
+#TODO: make fstop match more or less
+#TODO: add denoise step
 
 import os
 import sys
 import json
 import math
+import re
 
 # append path if not found
 try:
@@ -18,6 +21,54 @@ texture_search_path = "/Users/zeno/lentil/pota/tests/unit_render/tex/"
 car_ass = "/Users/zeno/lentil/pota/tests/unit_render/car_template01.ass"
 os.environ["UNITRENDER"] = "/Users/zeno/lentil/pota/tests/unit_render/"
 mtoa_plugins = "/Applications/solidangle/mtoa/2018/plugins"
+
+
+# yet to implement properly
+def denoise(filename_input):
+
+    # remove .exr
+    filename_input_base = filename_input[:-3]
+        
+    # only change these variables
+    filename_input = "shot.####.exr"
+    filename_output = "shot_denoiced.####.exr"
+    arnold_bin_path = "C:/solidangle/mtoadeploy/2018/bin"
+    startframe = 1
+    endframe = 1
+    variance = 0.5
+    pixel_search_radius = 9
+    pixel_neighborhood_patch_radius = 3
+    temporal_range = 6
+    light_aov_names = [] # if no light aov denoising needed, just leave list empty: []
+
+
+
+    filename_input_sequence_number = re.sub(r'#+',lambda m: r'{{:0{}d}}'.format(len(m.group(0))), filename_input)
+    filename_output_sequence_number = re.sub(r'#+',lambda m: r'{{:0{}d}}'.format(len(m.group(0))), filename_output)
+
+    for i in range (startframe, endframe + 1):
+        temporal_range_string = ""
+        light_aov_string = ""
+        for j in range(-temporal_range/2, temporal_range/2):
+            if (j==0) or (i+j < startframe) or (i+j > endframe):
+                continue
+            
+            temporal_range_string += "-i " + filename_input_sequence_number.format(i+j) + " "
+
+        for k in light_aov_names:
+            light_aov_string += "-aov " + str(k) + " "
+        
+        print arnold_bin_path + "/noice.exe " \
+                + "-patchradius " + str(pixel_neighborhood_patch_radius) + " " \
+                + "-searchradius " + str(pixel_search_radius) + " " \
+                + "-variance " + str(variance) + " " \
+                + light_aov_string + " " \
+                + "-i " + filename_input_sequence_number.format(i) + " " \
+                + temporal_range_string + " " \
+                + "-output " + filename_output_sequence_number.format(i),
+                
+        if i is not endframe:
+            print " &&",
 
 
 
@@ -62,6 +113,7 @@ def unit_render_lens(lensdict, lentil):
     AiEnd()
 
 
+
 def collect_all_prod_ready_lenses(lens_json):
     lens_global_dict = {}
 
@@ -71,8 +123,10 @@ def collect_all_prod_ready_lenses(lens_json):
     for lensid in lens_database:
         lens_dict = {}
         if lens_database[lensid]["production-ready"]:
-            for focallength in lens_database[lensid]["polynomial-optics"]:
 
+            for focallength in lens_database[lensid]["polynomial-optics"]:
+                focallength_dict = {}
+                
                 lens_name = "{company}_{name}_{year}_{focallength}mm".format(
                     company = lens_database[lensid]["company"].lower(),
                     name = lens_database[lensid]["product-name"].lower(),
@@ -87,9 +141,8 @@ def collect_all_prod_ready_lenses(lens_json):
                                                           focallength,
                                                           lens_name)
                 
-                lens_dict["focallength"] = focallength
-                lens_dict["lens_name"] = lens_name
-                lens_dict["outfile"] = outfile
+                focallength_dict["lens_name"] = lens_name
+                focallength_dict["outfile"] = outfile
 
                 lens_name_for_enum_find = "{company}__{name}__{year}__{focallength}mm".format(
                     company = lens_database[lensid]["company"].lower(),
@@ -97,9 +150,11 @@ def collect_all_prod_ready_lenses(lens_json):
                     year = lens_database[lensid]["year"],
                     focallength = focallength)
 
-                lens_dict["lens_name_for_enum_find"] = lens_name_for_enum_find.replace("-", "_")
-                
-                lens_global_dict[lensid] = lens_dict
+                focallength_dict["lens_name_for_enum_find"] = lens_name_for_enum_find.replace("-", "_")
+
+                lens_dict[focallength] = focallength_dict
+            
+            lens_global_dict[lensid] = lens_dict
     
     return lens_global_dict
 
@@ -107,10 +162,10 @@ def collect_all_prod_ready_lenses(lens_json):
 
 def execute_all():
     lenses = collect_all_prod_ready_lenses("/Users/zeno/lentil/polynomial-optics/database/lenses.json")
-    for lensid, info in lenses.items():
-        unit_render_lens(info, True)
-        unit_render_lens(info, False)
-        return
+    for lensid, focallength in lenses.items():
+        for focallength, info in focallength.items():
+            unit_render_lens(info, True)
+            unit_render_lens(info, False)
 
 def execute_single(lensid, focallength):
     lenses = collect_all_prod_ready_lenses("/Users/zeno/lentil/polynomial-optics/database/lenses.json")
